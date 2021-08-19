6b0e5e0b998e282dec974b819ce47c8ba1d93a9f
Nordicade <46762778+Nordicade@users.noreply.github.com>
2021-08-18 21:34:22 -0600

    Progress with work page draft and Nav bar update

---
+++ Controllers/WorkController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyWebsite.Models;
+
+namespace MyWebsite.Controllers
+{
+    public class WorkController : Controller
+    {
+        private readonly ILogger<WorkController> _logger;
+
+        public WorkController(ILogger<WorkController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Urc()
+        {
+            return View();
+        }
+
+        public IActionResult SpriteEditor()
+        {
+            return View();
+        }
+
+        public IActionResult DayPlanner()
+        {
+            return View();
+        }
+
+        public IActionResult LMS()
+        {
+            return View();
+        }
+
+        public IActionResult Blackjack()
+        {
+            return View();
+        }
+
+        public IActionResult ChessBrowser()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
